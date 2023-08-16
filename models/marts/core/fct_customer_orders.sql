5f4298398c9886c6bccae56b08afcfe1d5ad7ce0
Farid "Freddie" Taba <farid.t@gmail.com>
2023-08-16 20:14:21 +0000

    Simplify further by replacing joins with window functions

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -37,35 +37,28 @@ paid_orders as (
     left join customers on orders.user_id = customers.id 
 ),
 
-customer_orders as (
-    select customers.id as customer_id,
-        min(orders.order_date) as first_order_date,
-        max(orders.order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders
-    from customers 
-    left join orders
-    on orders.user_id = customers.id 
-    group by 1
-),
-
 final as (
     select
-        paid_orders.*,
-        row_number() over (order by paid_orders.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+        *,
+        row_number() over (order by order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
         case
-            when customer_orders.first_order_date = paid_orders.order_placed_at
-            then 'new'
-            else 'return' 
+            when rank() over (
+                partition by customer_id
+                order by order_placed_at, order_id
+            ) = 1 then 'new'
+            else 'return'
         end as nvsr,    -- new vs. returning customer
-        sum(paid_orders.total_amount_paid) over (
-            partition by paid_orders.customer_id
-            order by paid_orders.order_id
+        sum(total_amount_paid) over (
+            partition by customer_id
+            order by order_id
         ) as customer_lifetime_value,
-        customer_orders.first_order_date as fdos    -- first day of sale
+        first_value(order_placed_at) over(
+            partition by customer_id
+            order by order_placed_at
+        ) as fdos    -- first day of sale
     from paid_orders
-    left join customer_orders using (customer_id)
-    order by paid_orders.order_id
 )
 
 select * from final
+order by order_id
