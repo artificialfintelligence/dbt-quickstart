34dd60d67c58233bb23e4efe9eba2180dd897ae0
Farid "Freddie" Taba <farid.t@gmail.com>
2023-08-16 19:30:07 +0000

    Convert one more source which was missed earlier

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -42,32 +42,36 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(orders.id) as number_of_orders
-    from dbt-tutorial.jaffle_shop.customers c 
+    from customers c 
     left join orders
     on orders.user_id = c.id 
     group by 1
+),
+
+final as (
+    select
+        p.*,
+        row_number() over (order by p.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        case
+            when c.first_order_date = p.order_placed_at
+            then 'new'
+            else 'return' 
+        end as nvsr,
+        x.clv_bad as customer_lifetime_value,
+        c.first_order_date as fdos
+    from paid_orders p
+    left join customer_orders as c using (customer_id)
+    left outer join (
+        select
+            p.order_id,
+            sum(t2.total_amount_paid) as clv_bad
+        from paid_orders p
+        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+        group by 1
+        order by p.order_id
+    ) x on x.order_id = p.order_id
+    order by order_id
 )
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case
-        when c.first_order_date = p.order_placed_at
-        then 'new'
-        else 'return' 
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join (
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
-order by order_id
+select * from final
