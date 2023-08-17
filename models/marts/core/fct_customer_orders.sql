986dff4defc0ee9e19a7dccbd8157ad46b27278b
Farid "Freddie" Taba <farid.t@gmail.com>
2023-08-17 04:24:34 +0000

    Rewrite to source from new staging models

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -1,45 +1,52 @@
 with
 
 orders as (
-    select * from {{ source('jaffle_shop', 'orders') }}
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
 customers as (
-    select * from {{ source('jaffle_shop', 'customers') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 payments as (
-    select * from {{ source('stripe', 'payment') }}
+    select * from {{ ref('stg_stripe__payments') }}
 ),
 
 completed_payments as (
     select 
-        orderid as order_id,
-        max(created) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
+        order_id,
+        max(payment_created_at) as payment_finalized_date,
+        sum(payment_amount) as total_amount_paid
     from payments
-    where status <> 'fail'
+    where payment_status <> 'fail'
     group by 1
 ),
 
 paid_orders as (
     select 
-        orders.id as order_id,
-        orders.user_id	as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at,
+        orders.order_status,
         completed_payments.total_amount_paid,
         completed_payments.payment_finalized_date,
-        customers.first_name as customer_first_name,
-        customers.last_name as customer_last_name
+        customers.customer_first_name,
+        customers.customer_last_name
     from orders
-    left join completed_payments on orders.id = completed_payments.order_id
-    left join customers on orders.user_id = customers.id 
+    left join completed_payments on orders.order_id = completed_payments.order_id
+    left join customers on orders.customer_id = customers.customer_id 
 ),
 
 final as (
     select
-        *,
+        order_id,
+        customer_id,
+        order_placed_at,
+        order_status,
+        total_amount_paid,
+        payment_finalized_date,
+        customer_first_name,
+        customer_last_name,
         row_number() over (order by order_id) as transaction_seq,
         row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
         case
